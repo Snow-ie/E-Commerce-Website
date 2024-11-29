b4a5a0ccc37e60b2424a0feb758c71580b125bac
Ekeh Pearl <pearlekeh7@gmail>
2024-11-29 12:39:32 +0000

    commit

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
     <div className="pt-[80px]">
       <footer className="bg-primary2 text-primary">
         <div className="py-8">
-          <div className="container mx-auto px-4 grid gap-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+          <div className="container mx-auto px-4 grid gap-8 grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
             <div className="space-y-4">
               <h3 className="text-2xl font-bold">Exclusive</h3>
               <h4 className="text-2xl font-medium">Subscribe</h4>
